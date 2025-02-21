811ee16e6f222a1f04d2df9fb532f9fd7d6a7ee1
xcode1880 <xcode.mac.1980>
2025-02-21 19:39:13 +0200

    Initial Commit

---
+++ SnippetsAppNew/SnippetsAppNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnippetsAppNewApp.swift
+//  SnippetsAppNew
+//
+//  Created by Yevgeny Levin on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnippetsAppNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
